45d3b662d89fab2bc142b1b6066749dc7b89f5b2
Yiming Cao <yiming@whimslab.io>
2022-08-26 09:06:00 +0800

    Initial commit

---
+++ houdini.config.js
@@ -0,0 +1,7 @@
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "client": "./src/client",
+    "apiUrl": "https://swapi-graphql.netlify.app/.netlify/functions/index"
+}
+
+export default config
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { graphql } from '$houdini';
+    import client from '../client';
+    client.init();
+
+    const film = graphql`
+        query Film {
+            film(id: "ZmlsbXM6MQ==") {
+                id
+                title
+            }
+        }
+    `;
+
+    $: console.log($film?.data?.film);
+</script>
+
+{#if $film?.data}
+    <h1>Movie: {$film.data.film.title}</h1>
+{:else if $film?.errors}
+    <p>Error: {$film.errors[0].message}</p>
+{/if}
+<slot />
